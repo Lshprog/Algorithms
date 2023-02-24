a53497a9edec3bab2a9baff83bf03bcd93111b62
onisalex1@gmail.com <onisalex1@gmail.com>
2023-02-25 01:39:54 +0800

    l

--- Source.cpp
+++ Source.cpp
@@ -2,11 +2,11 @@
 
 int main() {
 	std::vector<double> vec{ 10.25, 23.1, 10.250, -5.25, 0.01,
-		0.726, 15.6, 14.81,-5.25, -23.578,-15,-17,-5.5 ,0};
+		0.726, 15.6,14.81,-5.25, -23.578,-15,-17,-5.5 ,0};
 	//std::vector<double> vec{ -23,-15,-17};
 	HashTable mynewtable = HashTable(vec);
-	mynewtable.hash(vec);
-	mynewtable.print();
+	bool result = mynewtable.hash(vec);
+	if(result) mynewtable.print();
 	/*mynewtable.searchelem(0.93);
 	mynewtable.searchelem(1.93);
 	mynewtable.searchelem(0.4);
