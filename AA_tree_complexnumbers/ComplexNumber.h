111b4ebb9bd0102a132cf5760fe56cdadca9a064
onisalex1@gmail.com <onisalex1@gmail.com>
2023-03-27 03:27:26 +0800

    added class ComplexNumber, added compare function

---
+++ AA_tree_complexnumbers/ComplexNumber.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+
+
+class ComplexNumber {
+	int rp;
+	int ip;
+
+public:
+	ComplexNumber(int rp = 0, int ip = 0) {
+		this->rp = rp;
+		this->ip = ip;
+	}
+
+	~ComplexNumber() {
+
+	}
+
+	int getIp() {
+		return this->ip;
+	}
+
+	int getRp() {
+		return this->rp;
+	}
+
+	
+	
+};
+
+bool compare_complex(ComplexNumber* num1,ComplexNumber* num2);
