ee88f18d26fdd6eb92fe836c61f5f9441f821932
Lshprog <onisalex1@gmail.com>
2023-05-03 12:16:41 +0300

    Added thesting for different amount of threads

--- Fast_Fourier_Transform_multithreaded/Fast_Fourier_Transform_multithreaded/test.cpp
+++ Fast_Fourier_Transform_multithreaded/Fast_Fourier_Transform_multithreaded/test.cpp
@@ -10,30 +10,37 @@ typedef std::complex<double> cd;
 TEST_CASE("Testing Fast Fourier Transform alogrithms") {
 
     srand(time(NULL));
-    int numberofiter = 20;
+    int numberofiter = 25;
     
-    for (int i = 0; i < numberofiter; i++) {
-        std::cout << "2 power of : "<< i << std::endl;
-        std::vector<cd> a = getRandomVec(pow(2,i));
+        for (int i = 0; i < numberofiter; i++) {
+            //std::cout << "2 power of : " << i << std::endl;
 
-        auto start = std::chrono::high_resolution_clock::now();
-        std::vector<cd> b = FFT::fft(a);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> ms_result = end - start;
-        std::cout << "Time non-multithread : " << ms_result.count() << std::endl;
+            std::cout << std::endl;
 
-        
-        auto start1 = std::chrono::high_resolution_clock::now();
-        std::vector<cd> b1 = MT_FFT::fft(a);
-        auto end1 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> ms_result1 = end1 - start1;
-        std::cout <<"Time multithread : " << ms_result1.count() << std::endl;
-        
-        std::cout<<std::endl;
+            std::vector<cd> a = getRandomVec(pow(2, i));
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            auto start = std::chrono::high_resolution_clock::now();
+            std::vector<cd> b = FFT::fft(a);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> ms_result = end - start;
+            std::cout << "----------- Time non-multithread : " << ms_result.count() << std::endl;
 
-        CHECK(b == b1);
-    }    
+            for (int j = 1; j < 6; j++) {
 
+                const int maxth = pow(2, j);
+                std::cout << std::endl;
+                std::cout << " MAX number of add threads: " << maxth << " ";
+
+                auto start1 = std::chrono::high_resolution_clock::now();
+                std::vector<cd> b1 = MT_FFT::fft(a, maxth);
+                auto end1 = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double, std::milli> ms_result1 = end1 - start1;
+                std::cout << "Time multithread : " << ms_result1.count() << std::endl;
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+                CHECK(b == b1);
+            }
+        }
+    
 }
