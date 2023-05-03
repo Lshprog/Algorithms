64f549a1a37b2a12da92e8e7c6b77ca861a6be22
Lshprog <onisalex1@gmail.com>
2023-05-03 12:15:58 +0300

    changed the implementation of threaded version

--- Fast_Fourier_Transform_multithreaded/Fast_Fourier_Transform_multithreaded/fftmultithread.h
+++ Fast_Fourier_Transform_multithreaded/Fast_Fourier_Transform_multithreaded/fftmultithread.h
@@ -7,25 +7,20 @@
 
 #include "fft.h"
 
-#define THREAD_NUMBER   16
-static std::mutex mut;
+//constexpr int THREAD_NUMBER = 16;
+
 
 namespace MT_FFT {
 
 	template<typename Complex>
-	std::vector<Complex> fftmultithread(std::vector<Complex>& coefs, int& current_thread_amount) {
+	std::vector<Complex> fftmultithread(std::vector<Complex>& coefs, int& current_thread_amount, const int& max_threads) {
+
+		static std::mutex mut;
 
 		int n = coefs.size();
 
 		if (n == 1)
 			return std::vector<Complex>(1, coefs[0]);
-
-		std::vector<Complex> w(n);
-
-		for (int i = 0; i < n; i++) {
-			double alpha = -2 * PI * i / n;
-			w[i] = Complex(cos(alpha), sin(alpha));
-		}
 
 		std::vector<Complex> A0(n / 2), A1(n / 2);
 
@@ -36,37 +31,35 @@ namespace MT_FFT {
 
 		}
 
+		std::vector<Complex> w(n);
 		std::vector<Complex> y0;
 		std::vector<Complex> y1;
 
-		/*if (current_thread_amount < THREAD_NUMBER) {
-			std::promise<std::vector<Complex>> p;
-			std::future<std::vector<Complex>> f = p.get_future();
-			std::thread t([&]() { p.set_value(fftmultithread<Complex>(A0, current_thread_amount)); });
-			t.detach();
-			current_thread_amount++;
-			y1 = FFT::fft(A1);
-			y0 = f.get();
-		}
-		else {
-			y0 = FFT::fft(A0);
-			y1 = FFT::fft(A1);
-		}*/
+		if (current_thread_amount < max_threads) {
+			
+			std::future<std::vector<Complex>> handle = std::async(std::launch::async, std::ref(fftmultithread<Complex>), std::ref(A0), std::ref(current_thread_amount), max_threads);
+			//std::thread t1(&calcRootsOfUnity<Complex>,coefs ,std::ref(w), n);
+			std::future<std::vector<Complex>> handle2 = std::async(std::launch::async, std::ref(fftmultithread<Complex>), std::ref(A1), std::ref(current_thread_amount), max_threads);
+			{
+				std::lock_guard<std::mutex> lock(mut);
+				current_thread_amount *= 2;
+			}
 
-		if (current_thread_amount < THREAD_NUMBER) {
-			std::future<std::vector<Complex>> handle = std::async(std::launch::async, std::ref(fftmultithread<Complex>), std::ref(A0), std::ref(current_thread_amount));
-			y1 = FFT::fft(A1);
+			FFT::calcRootsOfUnity<Complex>(coefs, w, n);
+			
 			y0 = handle.get();
-			std::lock_guard<std::mutex> lock(mut);
-			current_thread_amount++;
+			y1 = handle2.get();
+			
+			//t1.join();
+			
 		}
 		else {
+			FFT::calcRootsOfUnity<Complex>(coefs, w, n);
 			y0 = FFT::fft(A0);
 			y1 = FFT::fft(A1);
 		}
 
-
-
+		
 		std::vector<Complex> y(n);
 
 		for (int k = 0; k < n / 2; k++) {
@@ -79,9 +72,9 @@ namespace MT_FFT {
 	}
 
 	template<typename Complex>
-	std::vector<Complex> fft(std::vector<Complex>& coefs) {
-		int current_thread_amount = 0;
-		return fftmultithread<Complex>(coefs, current_thread_amount);
+	std::vector<Complex> fft(std::vector<Complex>& coefs,const int max_threads) {
+		int current_thread_amount = 1;
+		return fftmultithread<Complex>(coefs, current_thread_amount, max_threads);
 
 	}
 }
